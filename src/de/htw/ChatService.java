f280fc333098984bb119bc06942a0fbf1a23156d
Tim Bartsch <tibartschm@googlemail.com>
2012-06-06 10:01:05 +0200

    implemented ChatService start.

--- src/de/htw/ChatService.java
+++ src/de/htw/ChatService.java
@@ -2,41 +2,57 @@ package de.htw;
 
 import de.uni_trier.jane.basetypes.Address;
 import de.uni_trier.jane.basetypes.ServiceID;
+import de.uni_trier.jane.service.EndpointClassID;
 import de.uni_trier.jane.service.RuntimeService;
+import de.uni_trier.jane.service.neighbor_discovery.NeighborDiscoveryServiceStub;
+import de.uni_trier.jane.service.network.link_layer.LinkLayer_async;
 import de.uni_trier.jane.service.operatingSystem.RuntimeOperatingSystem;
 import de.uni_trier.jane.service.parameter.todo.Parameters;
 import de.uni_trier.jane.visualization.shapes.Shape;
 
 public class ChatService implements RuntimeService{
-
+	
+	private static ServiceID serviceId = new EndpointClassID(ChatService.class.getName());
+	private static ServiceID linkLayerId;
+	private static ServiceID neighborId;
+	private RuntimeOperatingSystem runtimeOperatingSystem;
+	private Address address;
+	private LinkLayer_async linkLayer;
+	private DSDVService_sync dsdvService;
+	
+	
+	public ChatService(ServiceID linkLayerId, ServiceID neighborId) {
+		this.linkLayerId = linkLayerId;
+		this.neighborId = neighborId;
+	}
+	
 	@Override
 	public ServiceID getServiceID() {
-		// TODO Auto-generated method stub
-		return null;
+		return serviceId;
 	}
 
 	@Override
 	public void finish() {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public Shape getShape() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void getParameters(Parameters parameters) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void start(RuntimeOperatingSystem runtimeOperatingSystem) {
-		// TODO Auto-generated method stub
-		
+		this.runtimeOperatingSystem = runtimeOperatingSystem;
+		NeighborDiscoveryServiceStub neighborDiscoveryServiceStub = new NeighborDiscoveryServiceStub(runtimeOperatingSystem, neighborId);
+		this.address = neighborDiscoveryServiceStub.getOwnAddress();
+		this.linkLayer = (LinkLayer_async)runtimeOperatingSystem.getSignalListenerStub(linkLayerId, LinkLayer_async.class);
+		runtimeOperatingSystem.registerAtService(linkLayerId, LinkLayer_async.class);
+		this.dsdvService = (DSDVService_sync)runtimeOperatingSystem.getSignalListenerStub(neighborId, DSDVService_sync.class);
+		this.runtimeOperatingSystem.registerAtService(neighborId, DSDVService_sync.class);
 	}
 
 	public void handleMessage(Address sender, String message, Address originSender,
