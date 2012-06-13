895782ccd8a7f2cd483333789aa5737f91b0f15a
Tim Bartsch <tibartschm@googlemail.com>
2012-06-13 13:42:25 +0200

    refactored var name

--- src/de/htw/ChatFrame.java
+++ src/de/htw/ChatFrame.java
@@ -25,7 +25,7 @@ import de.uni_trier.jane.basetypes.Address;
 public class ChatFrame extends java.awt.Frame implements Observer{
 	
 	private ChatService chatService;
-	private String ownAddressSring = "unknown" ;
+	private String ownAddressString = "unknown" ;
 
     /** Creates new form ChatFrame */
     public ChatFrame() {
@@ -83,7 +83,7 @@ public class ChatFrame extends java.awt.Frame implements Observer{
     	if(destination != null){
     		String message = this.messageTextField.getText();
     		chatService.sendMessage(message, destination);
-        	this.displayChatMessage(ownAddressSring, destination, message);
+        	this.displayChatMessage(ownAddressString, destination, message);
     	}
     }
     
@@ -128,9 +128,9 @@ public class ChatFrame extends java.awt.Frame implements Observer{
 			if(o instanceof DsdvService){
 				DsdvService dsdvService = (DsdvService)o;
 				Set<Address> contacts = dsdvService.getAllReachableDevices();
-				if (ownAddressSring == "unknown"){
-					ownAddressSring = dsdvService.getDeviceAddress().toString();
-					this.setTitle("Own device address: " + ownAddressSring);
+				if (ownAddressString == "unknown"){
+					ownAddressString = dsdvService.getDeviceAddress().toString();
+					this.setTitle("Own device address: " + ownAddressString);
 				}
 				updateContactList(contacts);
 			}else if(o instanceof ChatService){
